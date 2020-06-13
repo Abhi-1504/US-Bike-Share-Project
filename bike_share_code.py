b54681eb33f0c8a48babed9befe055674408c7f4
Abhilash Raj <abhilash.raj.me@gmail.com>
2020-06-13 17:10:41 +0530

    Reverting back the git changes made in git demo

--- bike_share_code.py
+++ bike_share_code.py
@@ -2,8 +2,6 @@ import time
 import pandas as pd
 import numpy as np
 
-# Just a single comment change
-
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
